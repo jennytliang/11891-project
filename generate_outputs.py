c2ef93f379a8fd41817ba22b2c03a797c72d130a
Jenny Ting Liang <jtliang@babel-login-1.lti.cs.cmu.edu>
2024-03-02 01:59:06 -0500

    Add initial code to have model generate solutions to HumanEval

---
+++ generate_outputs.py
@@ -0,0 +1,43 @@
+# pip install accelerate
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from datasets import load_dataset
+
+model_user = "codellama"
+model_name = "CodeLlama-7b-Python-hf"
+temperature = 4.0
+
+print("loading dataset...")
+dataset = load_dataset("openai_humaneval")
+
+print("loading tokenizer...")
+model_hf_name = f"{model_user}/{model_name}"
+tokenizer = AutoTokenizer.from_pretrained(model_hf_name)
+
+print("loading model...")
+model = AutoModelForCausalLM.from_pretrained(model_hf_name, device_map="auto")
+
+for i in range(10):
+    example = dataset["test"][i]
+    prompt = example["prompt"]
+
+    print("tokenizing...")
+    input_ids = tokenizer(prompt, return_tensors="pt").to("cuda")
+
+    print("generating outputs...")
+    outputs = model.generate(
+        **input_ids,
+        max_new_tokens=1000,
+        early_stopping=True,
+        num_beams=10,
+        num_return_sequences=10,
+        temperature=temperature,
+        do_sample=True
+    )
+
+    for j in range(len(outputs)):
+        decoded_output = tokenizer.decode(outputs[j]).replace("<s>", "").replace("</s>", "").lstrip()
+        print(decoded_output)
+        
+        f = open(f"./data/{model_name}/code/temperature-{temperature}_problem-{i}_solution-{j}.py", "w")
+        f.write(decoded_output)
+        f.close()
---
+++ generate_outputs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=debug-humaneval
+#SBATCH --output=debug-humaneval.out
+#SBATCH --error=debug-humaneval.err
+#SBATCH --time=1:00:00
+#SBATCH --gpus=2
+
+cd /home/jtliang/interactive-constrained-decoding
+module load cuda-12.1
+export CUDA_VISIBLE_DEVICES=0,1
+python3 get_outputs.py 
