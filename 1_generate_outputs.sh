9b1a9c7c862c09e7355edde22031c9572f870d97
Jenny Ting Liang <jtliang@babel-login-3.lti.cs.cmu.edu>
2024-03-28 01:42:39 -0400

    Update code generation scripts to utilize more GPUs

---
+++ 1_generate_outputs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=generate_outputs
+#SBATCH --output=debug-generate_outputs.out
+#SBATCH --error=debug-generate_outputs.err
+#SBATCH --time=5:00:00
+#SBATCH --gpus=6
+
+cd /home/jtliang/interactive-constrained-decoding/11891-project
+module load cuda-12.1
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5
+python3 1_generate_outputs.py 
