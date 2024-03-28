6da2be4b56853d23bd1369575ed5fdf1e21ea19b
Jenny Ting Liang <jtliang@babel-login-3.lti.cs.cmu.edu>
2024-03-28 01:43:28 -0400

    Add script to deduplicate code outputs

---
+++ 2_deduplicate_outputs.py
@@ -0,0 +1,25 @@
+# pip install accelerate
+from datasets import load_dataset
+
+model_user = "codellama"
+model_name = "CodeLlama-7b-Python-hf"
+temperature = 0.5
+num_outputs = 10
+
+print("loading dataset...")
+dataset = load_dataset("evalplus/humanevalplus")
+
+for i in range(len(dataset["test"])):
+    example = dataset["test"][i]
+    solutions = set()
+
+    for j in range(num_outputs):
+        f = open(f"./data/{model_name}/duplicated_code/temperature-{temperature}_problem-{i}_solution-{j}.py", "r")
+        code = f.read()
+
+        if code not in solutions:
+            solutions.add(code)
+            
+            f = open(f"./data/{model_name}/code/temperature-{temperature}_problem-{i}_solution-{j}.py", "w")
+            f.write(code)
+            f.close()
---
+++ 2_deduplicate_outputs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name=deduplicate_outputs
+#SBATCH --output=debug-deduplicate_outputs.out
+#SBATCH --error=debug-deduplicate_outputs.err
+#SBATCH --time=5:00:00
+
+cd /home/jtliang/interactive-constrained-decoding/11891-project
+module load cuda-12.1
+python3 2_deduplicate_outputs.py 
