96e81f7f369b22a6d8d07540878608f84d29a31f
Jenny Ting Liang <jtliang@babel-login-3.lti.cs.cmu.edu>
2024-04-01 19:54:19 -0400

    Convert running baseline 1 into a shell script

--- experiments/1_no_oracle_no_tests.py
+++ experiments/1_no_oracle_no_tests.py
@@ -1,60 +1,86 @@
 # pip install accelerate
 from datasets import load_dataset
+from utils import get_logger
+import argparse
 import subprocess
 import json
 
 model_user = "codellama"
 model_name = "CodeLlama-7b-Python-hf"
-temperatures = [0.5, 1.0, 2.0, 3.0, 4.0]
 
-def experiment():
-    print("loading dataset...")
+def test(logger, args):
+    t = args.temperature
+    logger.info(t)
+    input_folder_path, output_folder_path = args.folderpaths
+
+    logger.info("loading dataset...")
     dataset = load_dataset("evalplus/humanevalplus")
 
     data = {}
-    for t in temperatures:
-        print(f"computing temperature {t}...")
-        data[str(t)] = dict()
+    for i in range(len(dataset["test"])):
+        data[f"problem-{i}"] = []
 
-        for i in range(len(dataset["test"])):
-            data[str(t)][f"problem-{i}"] = []
+        example = dataset["test"][i]
+        test = example["test"]
+        entry_point = example["entry_point"]
 
-            example = dataset["test"][i]
-            test = example["test"]
-            entry_point = example["entry_point"]
+        for j in range(10):
+            try:
+                code_file_path = f"{input_folder_path}/temperature-{t}_problem-{i}_solution-{j}.py"
+                code_file = open(code_file_path, "r")
+                code = code_file.read()
+                code_file.close()
+            except: # Do nothing on error
+                pass
+            else:
+                test_file_path = f"{output_folder_path}/temperature-{t}_problem-{i}_solution-{j}.py"
+                test_file = open(test_file_path, "w")
+                test_file.write(code + "\n\n" + test + "\n\n" + f"check({entry_point})")
+                test_file.close()
 
-            for j in range(10):
+                # Build the command to run the Python script
+                # Assumes that 'python' is the command to run your Python interpreter
+                # This might need to be changed to 'python3' on some systems
+                command = ["python3", test_file_path]
+
+                # Execute the command
                 try:
-                    code_file_path = f"./data/{model_name}/code/temperature-{t}_problem-{i}_solution-{j}.py"
-                    code_file = open(code_file_path, "r")
-                    code = code_file.read()
-                    code_file.close()
-                except: # Do nothing on error
-                    pass
+                    process = subprocess.run(command, capture_output=True, text=True, timeout=120)
+                except Exception as e:
+                    data[f"problem-{i}"].append(str(e))
                 else:
-                    test_file_path = f"./experiments/out/{model_name}/no_oracle_no_tests/temperature-{t}_problem-{i}_solution-{j}.py"
-                    test_file = open(test_file_path, "w")
-                    test_file.write(code + "\n\n" + test + "\n\n" + f"check({entry_point})")
-                    test_file.close()
-
-                    # Build the command to run the Python script
-                    # Assumes that 'python' is the command to run your Python interpreter
-                    # This might need to be changed to 'python3' on some systems
-                    command = ["python3", test_file_path]
-
-                    # Execute the command
-                    try:
-                        process = subprocess.run(command, capture_output=True, text=True, timeout=120)
-                    except Exception as e:
-                        data[str(t)][f"problem-{i}"].append(str(e))
+                    # Check if the script was executed successfully
+                    if process.returncode == 0:
+                        data[f"problem-{i}"].append(1)
                     else:
-                        # Check if the script was executed successfully
-                        if process.returncode == 0:
-                            data[str(t)][f"problem-{i}"].append(1)
-                        else:
-                            data[str(t)][f"problem-{i}"].append(process.stderr)
+                        data[f"problem-{i}"].append(process.stderr)
 
-    print("writing to file...")
-    json.dump(data, open(f"./experiments/out/{model_name}/no_oracle_no_tests.json", "w"))
+    logger.info("writing to file...")
+    json.dump(data, open(f"{output_folder_path}/no_oracle_no_tests_{t}.json", "w"))
 
-experiment()
+def main():
+    # create parser object
+    parser = argparse.ArgumentParser(description = "A text file manager!")
+ 
+    # defining arguments for parser object
+    parser.add_argument("action")
+    
+    parser.add_argument("-f", "--folderpaths", type = str, nargs = 2,
+                        metavar = "folder_paths", default = None,
+                        help = "Reads input files and write output files to the specified folder paths (input, output)")
+    
+    parser.add_argument("-t", "--temperature", type = float,
+                        metavar = "temperature", default = None,
+                        help = "Temperature of the outputs")
+ 
+    # parse the arguments from standard input
+    args = parser.parse_args()
+     
+    # calling functions depending on type of argument
+    if args.action == "test":
+        logger = get_logger("INFO", "starting test for 1_no_oracle_no_tests.py")
+        test(logger, args)
+ 
+if __name__ == "__main__":
+    # calling the main function
+    main()
--- experiments/1_no_oracle_no_tests.sh
+++ experiments/1_no_oracle_no_tests.sh
@@ -4,6 +4,9 @@
 #SBATCH --error=debug-no_oracle_no_tests.err
 #SBATCH --time=5:00:00
 
+INPUT_FOLDER=/home/jtliang/interactive-constrained-decoding/11891-project/data/CodeLlama-13b-Python-hf/code
+OUTPUT_FOLDER=/home/jtliang/interactive-constrained-decoding/11891-project/experiments/out/CodeLlama-13b-Python-hf/no_oracle_no_tests
+
 cd /home/jtliang/interactive-constrained-decoding/11891-project
 module load cuda-12.1
-python3 ./experiments/1_no_oracle_no_tests.py
+python3 ./experiments/1_no_oracle_no_tests.py run -f $INPUT_FOLDER $OUTPUT_FOLDER -t 4.0
