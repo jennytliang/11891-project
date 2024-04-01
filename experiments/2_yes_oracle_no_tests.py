8e157dc66f537412edab2c7c9ff9f5252bff8ecc
Jenny Ting Liang <jtliang@babel-login-3.lti.cs.cmu.edu>
2024-04-01 19:55:02 -0400

    Add temperature param for shell script running baseline 2

--- experiments/2_yes_oracle_no_tests.py
+++ experiments/2_yes_oracle_no_tests.py
@@ -14,6 +14,7 @@ num_rounds = 1
 def run(logger, args):
     start_index, end_index = args.indicies
     input_folder_path, output_folder_path = args.folderpaths
+    t = args.temperature
     logger.info(f"start index: {start_index}, end index: {end_index}")
 
     logger.info("loading dataset...")
@@ -27,133 +28,130 @@ def run(logger, args):
     quantization_config = QuantoConfig(weights="int8")
     model = AutoModelForCausalLM.from_pretrained(model_hf_name, device_map="auto", quantization_config=quantization_config)
 
-    for t in temperatures:
-        logger.info(f"computing temperature {t}...")
+    logger.info(f"computing temperature {t}...")
 
-        for i in range(start_index, end_index):
-            example = dataset["test"][i]
-            prompt = example["prompt"].lstrip()
-            full_solution = prompt + example["canonical_solution"]
+    for i in range(start_index, end_index):
+        example = dataset["test"][i]
+        prompt = example["prompt"].lstrip()
+        full_solution = prompt + example["canonical_solution"]
 
-            for j in range(10):
-                try:
-                    code_file_path = f"{input_folder_path}/temperature-{t}_problem-{i}_solution-{j}.py"
-                    code_file = open(code_file_path, "r")
-                    full_code = code_file.read()
-                    code_file.close()
+        for j in range(10):
+            try:
+                code_file_path = f"{input_folder_path}/temperature-{t}_problem-{i}_solution-{j}.py"
+                code_file = open(code_file_path, "r")
+                full_code = code_file.read()
+                code_file.close()
 
-                    # Trim away everything past the specification, which could include constraints/things not related to code
-                    second_quote_end_index = get_second_quote_end_index(full_code)
-                    code = full_code[second_quote_end_index:]
-                except: # Do nothing on error
-                    pass
-                else:
-                    if code != full_solution:
-                        code_tokens = tokenizer.tokenize(code)
-                        solution_tokens = tokenizer.tokenize(example["canonical_solution"].lstrip())
-                        constraints = get_constraints(code_tokens, solution_tokens)
+                # Trim away everything past the specification, which could include constraints/things not related to code
+                second_quote_end_index = get_second_quote_end_index(full_code)
+                code = full_code[second_quote_end_index:]
+            except: # Do nothing on error
+                pass
+            else:
+                if code != full_solution:
+                    code_tokens = tokenizer.tokenize(code)
+                    solution_tokens = tokenizer.tokenize(example["canonical_solution"].lstrip())
+                    constraints = get_constraints(code_tokens, solution_tokens)
 
-                        constraint_text = None
-                        if len(constraints) > 0:
-                            constraint_to_use = constraints[0]
+                    constraint_text = None
+                    if len(constraints) > 0:
+                        constraint_to_use = constraints[0]
+                        constraint_text = get_constraint_text(constraint_to_use, code_tokens, solution_tokens, logger)
+
+                        # Find the next available constraint if the current constraint returns nothing
+                        index = 1
+                        logger.info(constraint_to_use)
+                        while constraint_text == "" and index < len(constraints):
+                            constraint_to_use = constraints[index]
                             constraint_text = get_constraint_text(constraint_to_use, code_tokens, solution_tokens, logger)
+                            index += 1
+                        logger.info(constraint_to_use)
+                        logger.info(constraint_text)
+                        
+                        # Identify tokens to keep
+                        solution_constraint_start_index = constraint_to_use[3] # Index of the constraint in solution_tokens
+                        logger.info(solution_tokens[0:solution_constraint_start_index])
+                        temp_code = tokenizer.decode(tokenizer.convert_tokens_to_ids(solution_tokens[0:solution_constraint_start_index]))
+                        logger.info(temp_code)
+                        
+                        assert(len(temp_code) <= len(example["canonical_solution"])) # temp_code should always be a subset of the canonical solution
 
-                            # Find the next available constraint if the current constraint returns nothing
-                            index = 1
-                            logger.info(constraint_to_use)
-                            while constraint_text == "" and index < len(constraints):
-                                constraint_to_use = constraints[index]
-                                constraint_text = get_constraint_text(constraint_to_use, code_tokens, solution_tokens, logger)
-                                index += 1
-                            logger.info(constraint_to_use)
-                            logger.info(constraint_text)
-                            
-                            # Identify tokens to keep
-                            solution_constraint_start_index = constraint_to_use[3] # Index of the constraint in solution_tokens
-                            logger.info(solution_tokens[0:solution_constraint_start_index])
-                            temp_code = tokenizer.decode(tokenizer.convert_tokens_to_ids(solution_tokens[0:solution_constraint_start_index]))
-                            logger.info(temp_code)
-                            
-                            assert(len(temp_code) <= len(example["canonical_solution"])) # temp_code should always be a subset of the canonical solution
+                        text_to_keep = code[0:len(temp_code)]
+                        logger.info(text_to_keep)
 
-                            text_to_keep = code[0:len(temp_code)]
-                            logger.info(text_to_keep)
+                    if constraint_text != None: # Should be None if the constraint is not valid
+                        closing_quotes = "\"\"\"" if "\"\"\"" in prompt else "'''"
 
-                        if constraint_text != None: # Should be None if the constraint is not valid
-                            closing_quotes = "\"\"\"" if "\"\"\"" in prompt else "'''"
+                        updated_code = prompt[:-4] + "\n" + constraint_text + f"\t{closing_quotes}\n" + text_to_keep
 
-                            updated_code = prompt[:-4] + "\n" + constraint_text + f"\t{closing_quotes}\n" + text_to_keep
+                        input_ids = tokenizer(updated_code, return_tensors="pt").to("cuda")
 
-                            input_ids = tokenizer(updated_code, return_tensors="pt").to("cuda")
+                        outputs = model.generate(
+                            **input_ids,
+                            max_new_tokens=500,
+                            temperature=t,
+                            # Do greedy decoding
+                            do_sample=True,
+                            num_beams=1
+                        )
 
-                            outputs = model.generate(
-                                **input_ids,
-                                max_new_tokens=500,
-                                temperature=t,
-                                # Do greedy decoding
-                                do_sample=True,
-                                num_beams=1
-                            )
+                        decoded_output = tokenizer.decode(outputs[0]).replace("<s>", "").replace("</s>", "").lstrip()
+                        
+                        f = open(f"{output_folder_path}/temperature-{t}_problem-{i}_solution-{j}.py", "w")
+                        f.write(decoded_output)
+                        f.close()
 
-                            decoded_output = tokenizer.decode(outputs[0]).replace("<s>", "").replace("</s>", "").lstrip()
-                            
-                            f = open(f"{output_folder_path}/temperature-{t}_problem-{i}_solution-{j}.py", "w")
-                            f.write(decoded_output)
-                            f.close()
-
-                            logger.info(f"{output_folder_path}/temperature-{t}_problem-{i}_solution-{j}.py")
+                        logger.info(f"{output_folder_path}/temperature-{t}_problem-{i}_solution-{j}.py")
 
 def test(logger, args):
     start_index, end_index = args.indicies
     input_folder_path, output_folder_path = args.folderpaths
+    t = args.temperature
 
     logger.info("loading dataset...")
     dataset = load_dataset("evalplus/humanevalplus")
 
     data = {}
-    for t in temperatures:
-        logger.info(f"computing temperature {t}...")
-        data[str(t)] = dict()
+    logger.info(f"computing temperature {t}...")
+    for i in range(start_index, end_index):
+        data[f"problem-{i}"] = []
 
-        for i in range(start_index, end_index):
-            data[str(t)][f"problem-{i}"] = []
+        example = dataset["test"][i]
+        for j in range(10):
+            try:
+                code_file_path = f"{input_folder_path}/temperature-{t}_problem-{i}_solution-{j}.py"
+                code_file = open(code_file_path, "r")
+                code = code_file.read()
+                code_file.close()
+            except: # Do nothing on error
+                pass
+            else:                            
+                test_file_path = f"{output_folder_path}/tests/temperature-{t}_problem-{i}_solution-{j}.py"
+                test_file = open(test_file_path, "w")
 
-            example = dataset["test"][i]
-            for j in range(10):
+                test = example["test"]
+                entry_point = example["entry_point"]
+
+                test_file_content = code + "\n\n" + test + "\n\n" + f"check({entry_point})"
+                test_file.write(test_file_content.encode("ascii", "ignore").decode("ascii"))
+                test_file.close()
+
+                # Build the command to run the Python script
+                # Assumes that 'python' is the command to run your Python interpreter
+                # This might need to be changed to 'python3' on some systems
+                command = ["python3", test_file_path]
+
+                # Execute the command
                 try:
-                    code_file_path = f"{input_folder_path}/temperature-{t}_problem-{i}_solution-{j}.py"
-                    code_file = open(code_file_path, "r")
-                    code = code_file.read()
-                    code_file.close()
-                except: # Do nothing on error
-                    pass
-                else:                            
-                    test_file_path = f"{output_folder_path}/tests/temperature-{t}_problem-{i}_solution-{j}.py"
-                    test_file = open(test_file_path, "w")
-
-                    test = example["test"]
-                    entry_point = example["entry_point"]
-
-                    test_file_content = code + "\n\n" + test + "\n\n" + f"check({entry_point})"
-                    test_file.write(test_file_content.encode("ascii", "ignore").decode("ascii"))
-                    test_file.close()
-
-                    # Build the command to run the Python script
-                    # Assumes that 'python' is the command to run your Python interpreter
-                    # This might need to be changed to 'python3' on some systems
-                    command = ["python3", test_file_path]
-
-                    # Execute the command
-                    try:
-                        process = subprocess.run(command, capture_output=True, text=True, timeout=120)
-                    except Exception as e:
-                        data[str(t)][f"problem-{i}"].append(str(e))
+                    process = subprocess.run(command, capture_output=True, text=True, timeout=120)
+                except Exception as e:
+                    data[f"problem-{i}"].append(str(e))
+                else:
+                    # Check if the script was executed successfully
+                    if process.returncode == 0:
+                        data[f"problem-{i}"].append(1)
                     else:
-                        # Check if the script was executed successfully
-                        if process.returncode == 0:
-                            data[str(t)][f"problem-{i}"].append(1)
-                        else:
-                            data[str(t)][f"problem-{i}"].append(process.stderr)
+                        data[f"problem-{i}"].append(process.stderr)
 
     logger.info("writing to file...")
     json.dump(data, open(f"{output_folder_path}/yes_oracle_no_tests.json", "w"))
@@ -172,6 +170,10 @@ def main():
     parser.add_argument("-i", "--indicies", type = int, nargs = 2,
                         metavar = "indicies", default = None,
                         help = "Only considers dataset values between the specified indicies (start inclusive, end exclusive)")
+    
+    parser.add_argument("-t", "--temperature", type = float,
+                        metavar = "temperature", default = None,
+                        help = "Temperature of the outputs")
  
     # parse the arguments from standard input
     args = parser.parse_args()
--- experiments/2_yes_oracle_no_tests_run.sh
+++ experiments/2_yes_oracle_no_tests_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=yes_oracle_no_tests_run1
-#SBATCH --output=debug-yes_oracle_no_tests_run1.out
-#SBATCH --error=debug-yes_oracle_no_tests_run1.err
+#SBATCH --job-name=8_yes_oracle_no_tests_run
+#SBATCH --output=debug-yes_oracle_no_tests_run_8.out
+#SBATCH --error=debug-yes_oracle_no_tests_run_8.err
 #SBATCH --time=15:00:00
 #SBATCH --gres=gpu:A6000:1
 #SBATCH --mem=40G
@@ -11,12 +11,13 @@ module load cuda-12.1
 export CUDA_VISIBLE_DEVICES=0
 INPUT_FOLDER=/home/jtliang/interactive-constrained-decoding/11891-project/data/CodeLlama-7b-Python-hf/code
 OUTPUT_FOLDER=/home/jtliang/interactive-constrained-decoding/11891-project/experiments/out/CodeLlama-7b-Python-hf/yes_oracle_no_tests/round1
+TEMPERATURE=1.0
 
-python3 ./experiments/2_yes_oracle_no_tests.py run -f $INPUT_FOLDER $OUTPUT_FOLDER -i 0 20
-# python3 ./experiments/2_yes_oracle_no_tests.py run -f $INPUT_FOLDER $OUTPUT_FOLDER -i 20 41
-# python3 ./experiments/2_yes_oracle_no_tests.py run -f $INPUT_FOLDER $OUTPUT_FOLDER -i 41 61
-# python3 ./experiments/2_yes_oracle_no_tests.py run -f $INPUT_FOLDER $OUTPUT_FOLDER -i 61 82
-# python3 ./experiments/2_yes_oracle_no_tests.py run -f $INPUT_FOLDER $OUTPUT_FOLDER -i 82 102
-# python3 ./experiments/2_yes_oracle_no_tests.py run -f $INPUT_FOLDER $OUTPUT_FOLDER -i 102 123
-# python3 ./experiments/2_yes_oracle_no_tests.py run -f $INPUT_FOLDER $OUTPUT_FOLDER -i 123 143
-# python3 ./experiments/2_yes_oracle_no_tests.py run -f $INPUT_FOLDER $OUTPUT_FOLDER -i 143 164
+# python3 ./experiments/2_yes_oracle_no_tests.py run -f $INPUT_FOLDER $OUTPUT_FOLDER -i 0 20 -t $TEMPERATURE
+# python3 ./experiments/2_yes_oracle_no_tests.py run -f $INPUT_FOLDER $OUTPUT_FOLDER -i 20 41 -t $TEMPERATURE
+# python3 ./experiments/2_yes_oracle_no_tests.py run -f $INPUT_FOLDER $OUTPUT_FOLDER -i 41 61 -t $TEMPERATURE
+# python3 ./experiments/2_yes_oracle_no_tests.py run -f $INPUT_FOLDER $OUTPUT_FOLDER -i 61 82 -t $TEMPERATURE
+# python3 ./experiments/2_yes_oracle_no_tests.py run -f $INPUT_FOLDER $OUTPUT_FOLDER -i 82 102 -t $TEMPERATURE
+# python3 ./experiments/2_yes_oracle_no_tests.py run -f $INPUT_FOLDER $OUTPUT_FOLDER -i 102 123 -t $TEMPERATURE
+# python3 ./experiments/2_yes_oracle_no_tests.py run -f $INPUT_FOLDER $OUTPUT_FOLDER -i 123 143 -t $TEMPERATURE
+python3 ./experiments/2_yes_oracle_no_tests.py run -f $INPUT_FOLDER $OUTPUT_FOLDER -i 143 164 -t $TEMPERATURE
