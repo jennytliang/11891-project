e924f666b08088b5c74cbf7bba4b9a131fa64de1
Jenny Ting Liang <jtliang@babel-login-3.lti.cs.cmu.edu>
2024-04-02 21:04:31 -0400

    Rename results files; get results of CodeBERT and CodeBLEU scores

--- analysis/code_bleu.sh
+++ analysis/code_bleu.sh
@@ -9,9 +9,9 @@
 cd /home/jtliang/interactive-constrained-decoding/11891-project
 module load cuda-12.1
 export CUDA_VISIBLE_DEVICES=0
-PREDS_FOLDER=/home/jtliang/interactive-constrained-decoding/11891-project/data/CodeLlama-7b-Python-hf/code
-# PREDS_FOLDER=/home/jtliang/interactive-constrained-decoding/11891-project/experiments/out/CodeLlama-7b-Python-hf/yes_oracle_no_tests/round1
-OUTPUT_FILE=/home/jtliang/interactive-constrained-decoding/11891-project/analysis/out/CodeLlama-7b-Python-hf/codebleu/temperature-4.0_round-0.json
-TEMPERATURE=4.0
+PREDS_FOLDER=/home/jtliang/interactive-constrained-decoding/11891-project/data/CodeLlama-13b-Python-hf/code
+# PREDS_FOLDER=/home/jtliang/interactive-constrained-decoding/11891-project/experiments/out/CodeLlama-13b-Python-hf/yes_oracle_no_tests/round1
+OUTPUT_FILE=/home/jtliang/interactive-constrained-decoding/11891-project/analysis/out/CodeLlama-13b-Python-hf/codebleu/temperature-2.0_round-0.json
+TEMPERATURE=2.0
 
 python3 ./analysis/code_bleu.py -f $PREDS_FOLDER -t $TEMPERATURE -o $OUTPUT_FILE
