d2d7a013723c2217684b60dd6f1130d4ad3bc5d0
Nishant Subramani <nishant.subramani23@gmail.com>
2024-05-03 16:43:13 -0400

    added interaction sbatch scripts

---
+++ experiments/interaction_baseline_sbatch_scripts/deepseek.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=deepseek-interaction # Job name
+#SBATCH --gres=gpu:A6000:1               # Request GPU resources
+#SBATCH --time=2:00:00             # Time limit hrs:min:sec
+
+eval "$(conda shell.zsh hook)"
+conda activate code_gen
+
+# Run your Python script
+datadir=../data_v2/
+step=1
+models=("deepseek-ai/deepseek-coder-1.3b-instruct" "deepseek-ai/deepseek-coder-6.7b-instruct")
+temperatures=("0.0" "0.5" "1.0" "2.0" "3.0")
+for model in "${models[@]}"
+do
+    for temperature in "${temperatures[@]}"
+    do
+        python3 baseline_with_interaction.py --data-dir=$datadir --model-name=$model --temperature=$temperature --interaction-step=$step
+        python3 convert_gen_dict_to_eval_harness.py --data-dir=$datadir --model-name=$model --temperature=$temperature --key-in-dict=generation_step_$step --interaction-step=$step
+        python3 evaluate_gens.py --data-dir=$datadir --model-name=$model --temperature=$temperature --key-in-dict=generation_step_$step --interaction-step=$step
+    done
+done
---
+++ experiments/interaction_baseline_sbatch_scripts/starcoder2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=starcoder-interaction # Job name
+#SBATCH --gres=gpu:A6000:1               # Request GPU resources
+#SBATCH --time=8:00:00             # Time limit hrs:min:sec
+
+eval "$(conda shell.zsh hook)"
+conda activate code_gen
+
+# Run your Python script
+datadir=../data_v2/
+step=1
+models=("bigcode/starcoder2-3b" "bigcode/starcoder2-7b" "bigcode/starcoder2-15b")
+temperatures=("0.0" "0.5" "1.0" "2.0" "3.0")
+for model in "${models[@]}"
+do
+    for temperature in "${temperatures[@]}"
+    do
+        python3 baseline_with_interaction.py --data-dir=$datadir --model-name=$model --temperature=$temperature --interaction-step=$step
+        python3 convert_gen_dict_to_eval_harness.py --data-dir=$datadir --model-name=$model --temperature=$temperature --key-in-dict=generation_step_$step --interaction-step=$step
+        python3 evaluate_gens.py --data-dir=$datadir --model-name=$model --temperature=$temperature --key-in-dict=generation_step_$step --interaction-step=$step
+    done
+done
