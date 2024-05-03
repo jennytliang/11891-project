4d0d6c1b0936fdaea07ad53b10c7a1d0c612046e
Nishant Subramani <nishant.subramani23@gmail.com>
2024-05-03 15:37:10 -0400

    debug

--- experiments/baseline_sbatch_scripts/code_llama_baseline.sh
+++ experiments/baseline_sbatch_scripts/code_llama_baseline.sh
@@ -12,7 +12,7 @@ models=("codellama/CodeLlama-7b-Python-hf" "codellama/CodeLlama-13b-Python-hf")
 temperatures=("0.0" "0.5" "1.0" "2.0" "3.0")
 for model in "${models[@]}"
 do
-    for temperature in "${$temperatures[@]}"
+    for temperature in "${temperatures[@]}"
     do
         python3 experiment_with_constraints.py --data-dir=$datadir --model-name=$model --temperature=$temperature
         python3 convert_gen_dict_to_eval_harness.py --data-dir=$datadir --model-name=$model --temperature=$temperature --key-in-dict=generation_step_0 
