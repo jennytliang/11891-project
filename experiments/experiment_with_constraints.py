2c663392aefdba1bbaec92a1a1bbdcde421a250b
Nishant Subramani <nishant.subramani23@gmail.com>
2024-05-04 17:53:40 -0400

    added constraints logic

--- experiments/experiment_with_constraints.py
+++ experiments/experiment_with_constraints.py
@@ -28,18 +28,33 @@ def get_span_diffs(s1_tokens: List[str], s2_tokens: List[str], get_first_nonequa
                 return diffs[0]
     return diffs
 
-def get_interaction(generation: str, solution: str, tokenizer, interaction_max_len_tokens: int = 10):
+def get_interaction(generation: str, solution: str, tokenizer, interaction_max_len_tokens: int = 10, get_deleted_tokens: bool = False):
     generation_tok = tokenizer.tokenize(generation)
     solution_tok = tokenizer.tokenize(solution)
     solution_len = len(solution_tok)
     first_span_diff = get_span_diffs(s1_tokens=generation_tok, s2_tokens=solution_tok, get_first_nonequal_span_diff=True)
     tag, i1, i2, j1, j2 = first_span_diff
 
-    interaction_tokens = solution_tok[:min(j1+interaction_max_len_tokens, solution_len)]
+    intervention_end = min(j1 + interaction_max_len_tokens, solution_len)
+
+    interaction_tokens = solution_tok[:intervention_end]
     interaction_token_ids = tokenizer.convert_tokens_to_ids(interaction_tokens)
     interaction_str = tokenizer.decode(interaction_token_ids)
 
-    return interaction_str
+    if get_deleted_tokens:
+        deleted_tokens = []
+        all_span_diffs = get_span_diffs(s1_tokens=generation_tok, s2_tokens=solution_tok)
+        for tag, i1, i2, j1, j2 in all_span_diffs:
+            if j2 > intervention_end:
+                break
+
+            if tag == 'delete':
+                for tok in generation_tok[i1:i2]:
+                    deleted_tokens.append(tok)
+    else:
+        deleted_tokens = None
+
+    return interaction_str, deleted_tokens
 
 def parse_arguments():
     # create parser object
@@ -93,14 +108,73 @@ def parse_arguments():
         type=int,
         required=True,
     )
+    
+    parser.add_argument(
+        "--constraints-mode",
+        type=str,
+        default='constraints',
+    )
+    
+    parser.add_argument(
+        "--upweight-factor",
+        type=float,
+        default=2.0,
+    )
+    
+    parser.add_argument(
+        "--downweight-factor",
+        type=float,
+        default=0.5,
+    )
 
     return parser.parse_args()
 
-def base_logit_processor(token_ids, logits, interaction_tokens, factor: float = 2):
-    if len(token_ids) == 0:
-        for token in list(set(interaction_tokens)):
-            logits[token] *= factor
+def base_logit_processor(token_ids, logits, interaction_tokens, deleted_tokens, excluded_tokens, upweight_factor: float = 2, downweight_factor: float = 0.5):
+    for token in list(set(deleted_tokens)):
+        if token not in excluded_tokens and token not in interaction_tokens:
+            logits[token] *= downweight_factor
+    for token in list(set(interaction_tokens)):
+        if token not in excluded_tokens and token not in deleted_tokens:
+            logits[token] *= upweight_factor
     return logits
+
+def get_excluded_tokens(tokenizer):
+    excluded_token_as_str = [
+        '\n',
+        '\t',
+        ';',
+        ':',
+        ',',
+        '!',
+        '@',
+        '#',
+        '$',
+        '%',
+        '^',
+        '&',
+        '*',
+        '(',
+        ')',
+        '[',
+        ']',
+        '{',
+        '}',
+        '/',
+        '.',
+        ' ',
+        'print',
+        'return',
+        'assert',
+        'for',
+        'while',
+        'pass',
+        'if',
+        'else', 
+    ]
+    excluded_tokens = []
+    for tok in excluded_token_as_str:
+        excluded_tokens.extend(tokenizer.encode(tok)[1:])
+    return excluded_tokens
 
 if __name__ == "__main__":
     args = parse_arguments()
@@ -123,7 +197,7 @@ if __name__ == "__main__":
     output_file_name = os.path.join(
         args.data_dir,
         "outputs",
-        f"{args.model_name.replace('/', '-')}_temp={args.temperature}_step={args.interaction_step}_constraints.dict",
+        f"{args.model_name.replace('/', '-')}_temp={args.temperature}_step={args.interaction_step}_{args.constraints_mode}.dict",
     )
 
     if os.path.exists(output_file_name):
@@ -140,7 +214,7 @@ if __name__ == "__main__":
         individual_results_path = os.path.join(
             args.data_dir,
             "outputs",
-            f"{args.model_name.replace('/', '-')}_temp={args.temperature}_stage=generation_step_{args.interaction_step-1}_constraints.eval_harness_gens.individual_results.json",
+            f"{args.model_name.replace('/', '-')}_temp={args.temperature}_stage=generation_step_{args.interaction_step-1}_{args.constraints_mode}.eval_harness_gens.individual_results.json",
         )
 
     if not os.path.exists(individual_results_path):
@@ -160,7 +234,7 @@ if __name__ == "__main__":
         prev_interaction_output_filename = os.path.join(
             args.data_dir,
             "outputs",
-            f"{args.model_name.replace('/', '-')}_temp={args.temperature}_step={args.interaction_step - 1}_constraints.dict",
+            f"{args.model_name.replace('/', '-')}_temp={args.temperature}_step={args.interaction_step - 1}_{args.constraints_mode}.dict",
         )
 
     with open(prev_interaction_output_filename, 'r') as f:
@@ -171,6 +245,8 @@ if __name__ == "__main__":
 
     # Load tokenizer
     tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+
+    excluded_tokens = get_excluded_tokens(tokenizer=tokenizer)
 
     # Run forward pass
     model = LLM(
@@ -200,6 +276,7 @@ if __name__ == "__main__":
     
     task_ids_not_already_passed = []
     interaction_tokens_list = []
+    deleted_tokens_list = []
 
     for task_id in prev_interaction_outputs_dict:
         prev_generation = prev_interaction_outputs_dict[task_id][f"generation_step_{args.interaction_step-1}"]
@@ -214,15 +291,17 @@ if __name__ == "__main__":
             if prev_interaction_step_key in prev_interaction_outputs_dict[task_id]:
                 prev_generation = prev_interaction_outputs_dict[task_id][prev_interaction_step_key] + prev_generation
 
-            interaction_str = get_interaction(generation=prev_generation, solution=curr_solution, tokenizer=tokenizer)
+            interaction_str, deleted_tokens = get_interaction(generation=prev_generation, solution=curr_solution, tokenizer=tokenizer, get_deleted_tokens=True)
             output_dict[task_id][f'interaction_{args.interaction_step}'] = interaction_str
 
             prefixes.append(
                 prev_interaction_outputs_dict[task_id]["prompt"].lstrip() + interaction_str
             )
 
-            interaction_tok = tokenizer.encode(interaction_str.replace('\n', '').replace('\t', ''))
+            interaction_tok = tokenizer.encode(interaction_str)[1:] # removes bos
+            deleted_tokens_tok = tokenizer.encode(deleted_tokens)[1:] # removes bos
             interaction_tokens_list.append(interaction_tok)
+            deleted_tokens_list.append(deleted_tokens_tok)
         else:
             output_dict[task_id][f"generation_step_{args.interaction_step}"] = prev_generation
             if prev_interaction_step_key in prev_interaction_outputs_dict[task_id]:
@@ -231,9 +310,16 @@ if __name__ == "__main__":
     # Save generations
 
     for i, prefix in enumerate(prefixes):
-        logit_processor = partial(base_logit_processor, interaction_tokens=interaction_tokens_list[i], factor=2.0)
+        logit_processor = partial(base_logit_processor, 
+                                  interaction_tokens=interaction_tokens_list[i],
+                                  deleted_tokens=deleted_tokens_list[i],
+                                  excluded_tokens=excluded_tokens, 
+                                  upweight_factor=args.upweight_factor,
+                                  downweight_factor=args.downweight_factor)
         sampling_params = SamplingParams(temperature=args.temperature, max_tokens=500, logits_processors=[logit_processor])
         output = model.generate([prefix], sampling_params)
+
+        import ipdb; ipdb.set_trace()
 
         task_id = task_ids_not_already_passed[i]
         output_dict[task_id][f"generation_step_{args.interaction_step}"] = output[0].outputs[0].text
